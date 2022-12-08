8a5f611081ca692e54a50f4a51eca1da035893bc
hojinHwang <imkoreanguy@gmail.com>
2022-12-09 06:27:01 +0900

    modified:   connector.py

--- connector.py
+++ connector.py
@@ -2,9 +2,94 @@
 
 import streamlit as st
 import database as db
-import send_sms
 import telegrambot
 import time
+import streamlit as st
+
+import hashlib
+import hmac
+import base64
+import requests
+import json
+
+
+
+##문자보내는 영역##########################################################################
+
+
+timestamp=int(time.time() * 1000)
+timestamp=str(timestamp)
+
+access_key = st.secrets.naver_sms.access_key			# access key id (from portal or Sub Account)
+secret_key = st.secrets.naver_sms.secret_key					# secret key (from portal or Sub Account)
+
+url="https://sens.apigw.ntruss.com"
+uri="/sms/v2/services/ncp:sms:kr:259200498077:connect/messages"
+
+
+def	make_signature():
+    global secret_key
+    global access_key
+    global timestamp
+    global url
+    global uri
+    secret_key = bytes(str(secret_key), encoding='utf-8')
+    method = "POST"
+    message = method + " " + uri + "\n" + timestamp + "\n" + access_key
+    message = bytes(message, encoding='utf-8')
+    signingKey = base64.b64encode(hmac.new(secret_key, message, digestmod=hashlib.sha256).digest())
+    return signingKey
+
+
+
+def send_sms(number, subject, contents):
+    header = {
+    "Content-Type": "application/json; charset=utf-8",
+    "x-ncp-apigw-timestamp": timestamp, 
+    "x-ncp-iam-access-key": access_key,
+    "x-ncp-apigw-signature-v2": make_signature()
+    }
+    data = {
+        "type":"LMS",
+        "from":"01030181959",
+        "countryCode":"82",
+        "content":contents,
+        "subject":subject,
+        "messages":[
+            {
+                "to":number,
+            }
+        ]
+    }
+    res = requests.post(url+uri,headers=header,data=json.dumps(data))
+    datas=json.loads(res.text)
+    print(res.text+"\n")
+    print(datas)
+    # reid=datas['requestId']
+
+    print("메시지 전송 상태")
+
+############################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ## 시간체크
 from datetime import datetime
@@ -145,7 +230,7 @@ if st.session_state['check_id']==True:
                 msg_header = "[보험커넥트]\n보험커넥트에 커넥트 회원가입이 완료되었습니다\n아래 전용 접속주소를 주위 고객분들에게 안내해주세요\n"
                 msg_content = msg_header+date_time+"\n"+"커넥터 가입내역\n"+msg +"\n"+"{}님 전용 접속주소 :\n {}".format(form_name,connect_url)
                 telegrambot.send_telegram(msg_content)
-                send_sms.send_sms(str(form_phone_num),"보험체크", msg_content)
+                send_sms(str(form_phone_num),"보험체크", msg_content)
                 # st.write(base_url)
                 st.success("{}님 보험커넥터 활동을 위한 링크주소 :\n".format(form_name))
                 st.success("{}\n".format(connect_url))
