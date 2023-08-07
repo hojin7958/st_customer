c74410a68bf272e22dc29f70eb15fee013390dcd
hojin7958 <imkoreanguy@gmail.com>
2023-08-07 19:15:57 +0900

    modified:   __pycache__/database.cpython-39.pyc 	modified:   app.py 	renamed:    connector.py -> connector_old.py 	renamed:    customer.py -> customer_old.py 	new file:   pages/1_connector.py 	new file:   pages/2_customer.py

--- app.py
+++ app.py
@@ -1,6 +1,44 @@
 import streamlit as st
 
-st.set_page_config(page_title="보험 커넥트 - 당신의 소득에 보험을 연결하세요",page_icon=":moneybag:")
+st.set_page_config(page_title="보험 커넥트 - 당신의 소득에 보험을 연결하세요",page_icon=":moneybag:", initial_sidebar_state="collapsed")
+
+### 여기부터는 햄버거 메뉴 없애는 곳들
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            [data-testid="collapsedControl"] {
+                display: none
+            }
+            footer {visibility: hidden;}
+             a:link {
+                text-decoration: none;
+            }
+
+            a:visited {
+                text-decoration: none;
+            }
+
+            a:hover {
+                text-decoration: none;
+            }
+
+            a:active {
+                text-decoration: none;
+            }
+
+            img {
+            width: 100%; /* takes the 100 % width of its container (.box div)*/
+            height: 100%; /* takes the 100 % height of its container (.box div)*/
+            # border : 1;
+            }
+
+
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
 
 
 st.header("보험 커넥트")
@@ -9,16 +47,4 @@ st.subheader("당신의 소득에 보험을 연결하세요")
 col1, col2 = st.columns([1,2])
 
 with col1:
-    st.write("야! 너두?")
-
-
-
-
-
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    st.write("NGIX기본페이지입니다")
